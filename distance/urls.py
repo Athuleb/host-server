5ac38a9b0c6c43749b6475f38e9f198dd594863d
Athuleb <athulbalaneb@gmail.com>
2024-11-13 13:09:08 +0530

    Remove all files from Git tracking

--- distance/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import FindDistance
-urlpatterns = [
-    path('',FindDistance.as_view(),name="FindDistance")
-]
--- sendemail/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import sendFeedbackClass
-
-
-urlpatterns = [
-    path('',sendFeedbackClass.as_view(),name='sendfeedback')
-]
