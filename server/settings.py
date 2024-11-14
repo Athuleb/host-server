591e7ce4baa0b29149ddecc8d831e3535c84ab0f
Athuleb <athulbalaneb@gmail.com>
2024-11-14 13:26:10 +0530

    db update

--- server/settings.py
+++ server/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hostdb_123',
+        'USER': 'athul', 
+        'PASSWORD': 'Wvz7Zo70AuixaEA0DDbZsPHCyz1tOYjM', 
+        'HOST': 'dpg-csqqopdumphs73d6u02g-a',  
+        'PORT': '5432',  
     }
 }
+
 
 
 # Password validation
